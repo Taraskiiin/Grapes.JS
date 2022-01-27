50996c7a19cfab5d6a02bbd1eb32f2cfc2932281
Taraskiiin <taraskravets95@gmail.com>
2022-01-27 23:32:38 +0200

    Feature(add)bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Router from "./Router/Router";
+import './styles.scss'
 
 const App = () => (
   <>
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react';
-import grapesjs from 'grapesjs';
-import 'grapesjs/dist/css/grapes.min.css';
-import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css';
-import 'grapesjs/dist/grapes.min.js';
-import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js';
+import React, { useEffect } from "react";
+import grapesjs from "grapesjs";
+import "grapesjs/dist/css/grapes.min.css";
+import "grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css";
+import "grapesjs/dist/grapes.min.js";
+import "grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js";
+import "grapesjs-blocks-bootstrap4";
 
 const Editor = () => {
-useEffect(() => {
+  useEffect(() => {
     grapesjs.init({
       container: "#gjs",
-      plugins: ["gjs-preset-webpage", "gjs-blocks-basic" ],
-      pluginsOpts: {
-        "gjs-blocks-basic": true,
+      plugins: ["gjs-preset-webpage", "grapesjs-blocks-bootstrap4"],
+      canvas: {
+        styles: [
+          "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css",
+        ],
+        scripts: [
+          "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js",
+          "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
+          "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js",
+        ],
       },
     });
-  },[]);
+  }, []);
 
   return (
     <>
-     <div id="gjs"></div>
+      <div id='gjs'></div>
     </>
   );
-}
+};
 
 export default Editor;
